60ebc1d8279e783b8b24387aefcfa6309e3dae6f
Abeer Sahu <abeer.sahu18@gmail.com>
2026-05-09 19:00:39 +0530

    Add files via upload

---
+++ Q30.cpp
@@ -0,0 +1,32 @@
+/*
+30.Write a program that prints all numbers between 1 and 500, skipping
+numbers divisible by both 3 and 7.
+ Stop printing numbers if the sum of printed numbers exceeds 1000.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Numbers-" << endl;
+    int sum = 0;
+
+    for (int i = 1; i <= 500; i++)
+    {
+        if (i % 21 == 0)
+        {
+            continue;
+        }
+
+        if (sum + i > 1000)
+        {
+            break;
+        }
+
+        cout << i << " ";
+        sum += i;
+    }
+
+    cout << "\nFinal Sum = " << sum << endl;
+    return 0;
+}
